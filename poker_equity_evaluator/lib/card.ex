95d58f76862d88d3e7d4702b7bf9bbf80021fc2c
Alan Voss <avoss@rent.com>
2016-03-18 18:45:28 -0700

    refactoring deck to have cards

---
+++ poker_equity_evaluator/lib/card.ex
@@ -0,0 +1,21 @@
+defmodule Card do
+  defstruct rank: "", suit: ""
+
+  @suits %{
+    s: %{ name: 'SPADES',   char: "♠" },
+    h: %{ name: 'HEARTS',   char: "♥" },
+    d: %{ name: 'DIAMONDS', char: "♦" },
+    c: %{ name: 'CLUBS',    char: "♣" }
+  }
+
+  def rank(%Card{rank: 1 }),   do: 'A'
+  def rank(%Card{rank: 10}),   do: 'T'
+  def rank(%Card{rank: 11}),   do: 'J'
+  def rank(%Card{rank: 12}),   do: 'Q'
+  def rank(%Card{rank: 13}),   do: 'K'
+  def rank(%Card{rank: rank}), do: rank
+
+  def suit(%Card{suit: suit}), do: @suits[suit][:name]
+
+  def suit_character(%Card{suit: suit}), do: @suits[suit][:char]
+end
--- poker_equity_evaluator/lib/deck.ex
+++ poker_equity_evaluator/lib/deck.ex
@@ -1,22 +1,4 @@
 defmodule Deck do
-  def deck, do: for suit <- [:s, :h, :d, :c], rank <- 1..13, do: {rank, suit}
-
-  def card({rank, suit}), do: {rank(rank), suit(suit)}
-
-  def rank(1),  do: 'A'
-  def rank(10), do: 'T'
-  def rank(11), do: 'J'
-  def rank(12), do: 'Q'
-  def rank(13), do: 'K'
-  def rank(rank), do: rank
-
-  def suit(:s), do: "SPADES"
-  def suit(:h), do: "HEARTS"
-  def suit(:d), do: "DIAMONDS"
-  def suit(:c), do: "CLUBS"
-
-  def suit_character(:s), do: "♠"
-  def suit_character(:h), do: "♥"
-  def suit_character(:d), do: "♦"
-  def suit_character(:c), do: "♣"
+  def deck, do: for suit <- [:s, :h, :d, :c], rank <- 1..13, do: %Card{rank: rank, suit: suit}
+  def remove(deck, cards), do: Enum.reduce(cards, deck, fn(card, deck) -> List.delete(deck, card) end)
 end
