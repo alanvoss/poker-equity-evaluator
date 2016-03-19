93d1cfcbf0c30d14ed4c767da3809fb839b01196
Alan Voss <avoss@rent.com>
2016-03-18 19:04:23 -0700

    adding combinations stream

--- poker_equity_evaluator/lib/deck.ex
+++ poker_equity_evaluator/lib/deck.ex
@@ -1,4 +1,10 @@
 defmodule Deck do
   def deck, do: for suit <- [:s, :h, :d, :c], rank <- 1..13, do: %Card{rank: rank, suit: suit}
   def remove(deck, cards), do: Enum.reduce(cards, deck, fn(card, deck) -> List.delete(deck, card) end)
+
+  def combinations(_, 0), do: [[]]
+  def combinations([], _), do: []
+  def combinations(_deck = [x|xs], n) when is_integer(n) do
+    (for y <- combinations(xs, n - 1), do: [x|y]) ++ combinations(xs, n)
+  end
 end
