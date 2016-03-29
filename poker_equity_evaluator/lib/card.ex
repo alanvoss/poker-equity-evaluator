a008167d1c0b12e5d1e8c9c2f5ca9799760a1b76
Alan Voss <alanvoss@hotmail.com>
2016-03-28 21:23:42 -0700

    refactoring Card to include ways to generate Card structs

--- poker_equity_evaluator/lib/card.ex
+++ poker_equity_evaluator/lib/card.ex
@@ -2,20 +2,39 @@ defmodule Card do
   defstruct rank: "", suit: ""
 
   @suits %{
-    s: %{ name: 'SPADES',   char: "♠" },
-    h: %{ name: 'HEARTS',   char: "♥" },
-    d: %{ name: 'DIAMONDS', char: "♦" },
-    c: %{ name: 'CLUBS',    char: "♣" }
+    s: %{ name: "SPADES",   char: "♠" },
+    h: %{ name: "HEARTS",   char: "♥" },
+    d: %{ name: "DIAMONDS", char: "♦" },
+    c: %{ name: "CLUBS",    char: "♣" }
   }
+
+  @rank_abbreviations %{
+    10 => "T",
+    11 => "J",
+    12 => "Q",
+    13 => "K",
+    14 => "A"
+  }
+
+  @rank_translations Map.merge(
+    Map.new(Enum.map(Map.to_list(@rank_abbreviations), fn({k, v}) -> {v, k} end)),
+    Map.new(Enum.map(2..9, fn(rank) -> {<<rank + 48>>, rank} end))
+  )
 
   def suits, do: Map.keys(@suits)
 
-  def rank(%Card{rank: 10}),   do: 'T'
-  def rank(%Card{rank: 11}),   do: 'J'
-  def rank(%Card{rank: 12}),   do: 'Q'
-  def rank(%Card{rank: 13}),   do: 'K'
-  def rank(%Card{rank: 14}),   do: 'A'
-  def rank(%Card{rank: rank}), do: rank
+  def rank(%Card{rank: rank}), do: @rank_abbreviations[rank] || rank
+
+  def card(<<rank :: utf8, suit :: utf8>>), do: card({<<rank>>, String.downcase(<<suit>>)})
+  def card([rank, suit]), do: card({<<rank>>, String.downcase(<<suit>>)})
+  def card({rank, suit}) when is_bitstring(rank), do: card({@rank_translations[rank] || rank, suit})
+  def card({rank, suit}) when is_bitstring(suit), do: card({rank, String.to_atom(String.downcase(suit))})
+  def card({[rank], suit}), do: card({<<rank>>, suit})
+  def card({rank, [suit]}), do: card({rank, <<suit>>})
+  def card({rank, suit}) do
+    suit = Enum.find(Map.keys(@suits), fn(s) -> suit == s end)
+    if suit && rank <= 14 && rank >= 2, do: %Card{rank: rank, suit: suit}, else: nil
+  end
 
   def suit(%Card{suit: suit}), do: @suits[suit][:name]
 
--- poker_equity_evaluator/test/card_test.exs
+++ poker_equity_evaluator/test/card_test.exs
@@ -10,7 +10,7 @@ defmodule CardTest do
   end
 
   test "that the name of the suit is returned" do
-    assert suit(@ace_of_spades) == 'SPADES'
+    assert suit(@ace_of_spades) == "SPADES"
   end
 
   test "the suit character is returned" do
@@ -22,6 +22,27 @@ defmodule CardTest do
   end
 
   test "the alpha rank is returned" do
-    assert rank(@ace_of_spades) == 'A'
+    assert rank(@ace_of_spades) == "A"
+  end
+
+  test "a card can be formed with a bitstring" do
+    assert card("AS") == %Card{suit: :s, rank: 14}
+    assert card("9d") == %Card{suit: :d, rank: 9}
+  end
+
+  test "a card can be formed with a charlist" do
+    assert card('TD') == %Card{suit: :d, rank: 10}
+    assert card('2h') == %Card{suit: :h, rank: 2}
+  end
+
+  test "a card can be formed with a tuple" do
+    assert card({"K", 'S'}) == %Card{suit: :s, rank: 13}
+    assert card({2, "d"}) == %Card{suit: :d, rank: 2}
+    assert card({5, :d}) == %Card{suit: :d, rank: 5}
+  end
+
+  test "a card can be formed with a combination of a tuple and a charlist" do
+    assert card({'Q', 'D'}) == %Card{suit: :d, rank: 12}
+    assert card({2, "d"}) == %Card{suit: :d, rank: 2}
   end
 end
