5d3d5517062011e7903d9e76facbc614f8a0142e
Alan Voss <avoss@rent.com>
2016-03-31 11:00:28 -0700

    fixing problem with equivalent hand detection

--- poker_equity_evaluator/lib/equity.ex
+++ poker_equity_evaluator/lib/equity.ex
@@ -1,25 +1,13 @@
 defmodule HoldEmEquity do
   def hole_cards(sets_of_cards) do
     winning_counts =
-      Enum.reduce(sets_of_cards, Deck.deck, fn(set, deck) -> deck -- set end)
+      Deck.deck -- List.flatten(sets_of_cards)
       |> Deck.combinations(5)
       |> Enum.reduce(Enum.map(sets_of_cards, fn _ -> 0 end), fn(combination, winners) ->
-           ranks = [winner | _] =
-             Enum.map(sets_of_cards, fn(set) -> Hand.high(combination ++ set) end)
-             |> Enum.sort(&Hand.high_sorter(&1, &2))
-
-new_winners =           Enum.reduce_while(Enum.with_index(ranks), winners, fn({rank, index}, winners)
-             -> if rank == winner do
-require IEx
-if index > 0, do: IEx.pry
-                  {:cont, List.update_at(winners, index, &(&1 + 1))}
-                else
-                  {:halt, winners}
-                end
-             end)
-#require IEx
-#IEx.pry
-new_winners
+           ranks = Enum.map(sets_of_cards, fn(set) -> Hand.high(combination ++ set) end)
+           [winner | _] = Enum.sort(ranks, &Hand.high_sorter(&1, &2))
+           Enum.reduce(Enum.with_index(ranks), winners, fn({rank, index}, winners)
+             -> if rank == winner, do: List.update_at(winners, index, &(&1 + 1)), else: winners end)
          end)
      sum = Enum.sum(winning_counts)
 require IEx
